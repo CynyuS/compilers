880368233717fd4d25f65c5fd6e948d012433711
Cynthia Shao <cynyu.shao@gmail.com>
2025-11-24 00:10:29 -0500

    finished tracing

--- tracer/lvn.py
+++ tracer/lvn.py
@@ -15,7 +15,11 @@ class Table_Occ(Enum):
      REPLACE = 3
      PRINT_RET = 4
      DONT_USE = 5
-     
+
+
+# with open(sys.argv[1], 'r') as file:
+#     instrs = json.load(file)
+instrs = json.load(sys.stdin)
 
 commutative_instr = ["call", "add", "mul", "and", "or", "eq", "neq"]
 #Make var a sort of enum, make var a str, and make idx a num
@@ -54,37 +58,6 @@ class LVN_Table:
     __repr__ = __str__
 
 
-class Block:
-	def __init__(self, idx, instrs):
-		self.idx = idx
-		self.instrs = instrs
-		self.edges = []
-
-	def label(self):
-		if self.instrs and "labels" in self.instrs[0]:
-			return self.instrs[0]["labels"]
-		return None
-	
-	def last(self):
-		if self.instrs:
-			return self.instrs[-1]
-		return None
-
-	def add_edge(self, target):
-		if target not in self.edges:
-			self.edges.append(target)
-	
-	def __str__(self):
-		return f"Block(idx={self.idx}, label={Block.label(self)}, edges = {self.edges} )"          
-	__repr__ = __str__
-# with open(sys.argv[1], 'r') as file:
-# 	instrs = json.load(file)
-instrs = json.load(sys.stdin)
-
-blocks = []
-block = []
-selfIdx = 0
-
 def split_func_calls(funcs): #get funcy
     res = ""
     for (i,func) in enumerate(funcs):
@@ -93,6 +66,68 @@ def split_func_calls(funcs): #get funcy
         else:
             res += func
     return res
+
+# GLOBAL INSTANTIATIONS
+
+# basic block arrays
+blocks = []
+block = []
+
+# function headers to basic blocks that are headers
+func_headers = {}
+
+# function to variables mappings
+func_vars = {}
+func_defs = {}
+
+# function to basic blocks under that function mappings
+func_blocks = {}  # func_blocks[func_name] = [list of blocks in that function]
+block_ref_count = {}
+label_to_block = {}
+
+# function to cfg mapping, just constructing cfg out of blocks yields a forest
+# want to label the forest by function
+func_cfg = {}
+
+# function to dominators mapping
+func_dom = {}
+# function to predecessors mapping
+func_preds = {}
+
+# function to sorted dominator set mapping
+func_sorted_dom = {}
+
+# function to dominance frontier mapping
+func_DF = {}
+
+# BASIC BLOCK DEFS
+
+class Block:
+    def __init__(self, idx, instrs, is_func_header):
+        self.idx = idx
+        self.instrs = instrs
+        self.edges = []
+        self.is_header = is_func_header
+
+    def label(self):
+        if self.instrs and "labels" in self.instrs[0]:
+            return self.instrs[0]["labels"]
+        return None
+    
+    def last(self):
+        if self.instrs:
+            return self.instrs[-1]
+        return None
+
+    def add_edge(self, target):
+        if target not in self.edges:
+            self.edges.append(target)
+    
+    def __str__(self):
+        return f"Block(idx={self.idx}, label={Block.label(self)}, edges = {self.edges} )"          
+    __repr__ = __str__
+
+# CONSTRUCTING BASIC BLOCKS
 
 def get_block_name(self,lbl):
     if self: 
@@ -108,75 +143,192 @@ def get_block_name(self,lbl):
     else:
         return lbl
 
-# creating basic blocks implementation
+def name_in_blocks(name):
+    if name == "": 
+        return False
+    for b in blocks: 
+        if b.idx == name:
+            return True 
+    return False
+
+# ==== INSTANTIATING GLOBAL BASIC BLOCKS ====
+
 for func in instrs["functions"]:
     if "instrs" in func:
-        for instr in func["instrs"]:  # loop over instructions in the function
+        for (i, instr) in enumerate(func["instrs"]):
+            if i == 0:
+                is_func_header = True
             if "label" in instr:
-                b0 = Block(get_block_name(block, instr["label"]), block)
-                selfIdx += 1
-                blocks.append(b0)
-                block = []
+                # Save the previous block if it exists
+                if block:
+                    name = get_block_name(block, "")
+                    b0 = Block(name, block, is_func_header)
+                    if is_func_header == True:
+                        b0 = Block("f" + func["name"], block, is_func_header)
+                    is_func_header = False
+                    blocks.append(b0)
+                    block = []
+                # Start new block with the label
                 block.append(instr)
             elif "op" in instr:
                 if instr["op"] == "br" or instr["op"] == "jmp" or instr["op"] == "ret":
-                    b1 = Block(get_block_name(block, ""), block)
-                    selfIdx += 1
                     block.append(instr)
+                    name = get_block_name(block, "")
+                    b1 = Block(name, block, is_func_header)
+                    if is_func_header == True:
+                        b1 = Block("f" + func["name"], block, is_func_header)
+                    is_func_header = False
                     blocks.append(b1)
                     block = []
                 else:
-                    selfIdx += 1
                     block.append(instr)
+        # Handle remaining instructions
         if block:
-            b2 = Block(get_block_name(block, ""), block)
-            selfIdx += 1
+            name = get_block_name(block, "")
+            b2 = Block(name, block, is_func_header)
+            if is_func_header == True:
+                b2 = Block("f" + func["name"], block, is_func_header)
+            is_func_header = False
             blocks.append(b2)
             block = []
+
+# Clean up empty blocks
+cleanup_arr = []
+for (i, block) in enumerate(blocks):
+    if block.idx != "":
+        cleanup_arr.append(block)
+blocks = cleanup_arr
+
+# FUNCTION PROCESSING
+
+# Group blocks by function
+for block in blocks:
+    if block.is_header:
+        current_func = block.idx
+        func_headers[current_func] = block
+        func_blocks[current_func] = [block]  # Include the header block itself
+    else:
+        if current_func:
+            func_blocks[current_func].append(block)
+
+# block to function lookup
+block_map = {block.idx: block for block in blocks}  # Global block map
+func_block_maps = {}  # Per-function block maps
+
+for func_name, func_block_list in func_blocks.items():
+    func_block_maps[func_name] = {block.idx: block for block in func_block_list}
+
+def get_block(block_idx, func_name=None):
+    if func_name and func_name in func_block_maps:
+        return func_block_maps[func_name].get(block_idx)
+    else:
+        return block_map.get(block_idx)
+
+# LABEL MAPPING POST BLOCK PROCESSING
+
+for func_name, func_blocks_list in func_blocks.items():
+    for block in func_blocks_list:
+        # Check if this block has a label
+        if block.instrs and "label" in block.instrs[0]:
+            label_name = block.instrs[0]["label"]
+            label_to_block[label_name] = block.idx
+
+# VARIABLE PROCESSING
+
+# for each function create the variables
+for func_name, blocks in func_blocks.items():
+    variables = set()
+    func_defs[func_name] = {}
     
-def probe_next(block):
+    # First, add function arguments as variables
+    # Find the original function definition to get its arguments
+    for orig_func in instrs["functions"]:
+        if "f" + orig_func["name"] == func_name or orig_func["name"] == func_name:
+            if "args" in orig_func:
+                for arg in orig_func["args"]:
+                    arg_name = arg["name"]
+                    arg_type = arg["type"]
+                    variables.add((arg_name, json.dumps(arg_type, sort_keys=True)))
+                    # Function arguments are "defined" at the entry block
+                    func_defs[func_name][arg_name] = [blocks[0].idx]
+            break
+    
+    # Then process instructions in blocks
+    for block in blocks:
+        for instr in block.instrs:
+            if "dest" in instr and "type" in instr:
+                var_name = instr["dest"]
+                var_type = instr["type"]
+                variables.add((var_name, json.dumps(var_type, sort_keys=True)))
+                # Initialize the list if this is the first time we see this variable
+                if var_name not in func_defs[func_name]:
+                    func_defs[func_name][var_name] = []
+                # Add the block index (not the block object) to the list
+                func_defs[func_name][var_name].append(block.idx)
+    
+    func_vars[func_name] = variables
+
+# CFG PROCESSING
+
+def probe_next_in_func(block_idx, func_blocks_list):
+    """Find the next block within a specific function"""
     found = False
-    for (i,b) in enumerate(blocks):
-        if found: 
-            if b.idx != block:
-                return b.idx
-        if (b.idx) == block:
+    for (i, b) in enumerate(func_blocks_list):
+        if found and b.idx != block_idx:
+            return b.idx
+        if b.idx == block_idx:
             found = True
+    return None
 
-# building the control flow graph edge list!
-cfg = {}
-for b in blocks:
-	last = b.last() # last instr in block, doesn't work if blocks empty
-	if last is not None:
-		if "op" in last:
-			if last["op"] == "jmp":
-				if "labels" in last: 
-					cfg[b.idx] = last["labels"]
-				else:
-					cfg[b.idx] = [last["dest"]]
-			elif last["op"] == "br":
-				cfg[b.idx] = [last["labels"][0], last["labels"][1]]    
-			elif last["op"] == "ret":
-				cfg[b.idx] = []
-			elif "dest" not in last and "labels" not in last:
-				cfg[b.idx] = []
-			else:
-				cfg[b.idx] = [probe_next(b.idx)]
-		elif "dest" not in last and "labels" not in last:
-			cfg[b.idx] = []
-		else:
-			cfg[b.idx] = [probe_next(b.idx)]
+def build_cfg_for_func(func_name, func_blocks_list):
+    """Build CFG for a single function"""
+    cfg = {}
+    
+    for block in func_blocks_list:
+        last = block.last()
+        if last is not None and "op" in last:
+            if last["op"] == "jmp":
+                target_label = last["labels"][0]
+                # Map label to actual block name
+                target_block = label_to_block.get(target_label, target_label)
+                cfg[block.idx] = [target_block]
+            elif last["op"] == "br":
+                label1 = last["labels"][0]
+                label2 = last["labels"][1]
+                # Map labels to actual block names
+                target1 = label_to_block.get(label1, label1)
+                target2 = label_to_block.get(label2, label2)
+                cfg[block.idx] = [target1, target2]
+            elif last["op"] == "ret":
+                cfg[block.idx] = []
+            else:
+                # Fall through to next block
+                next_block = probe_next_in_func(block.idx, func_blocks_list)
+                if next_block:
+                    cfg[block.idx] = [next_block]
+                else:
+                    cfg[block.idx] = []
+        else:
+            # Fall through to next block
+            next_block = probe_next_in_func(block.idx, func_blocks_list)
+            if next_block:
+                cfg[block.idx] = [next_block]
+            else:
+                cfg[block.idx] = []
+    
+    return cfg
 
-         
-
-
+# Create function-based CFGs
+for func_name, func_blocks_list in func_blocks.items():
+    func_cfg[func_name] = build_cfg_for_func(func_name, func_blocks_list)
 
 var2num = {}
 lvn_list = []
 full_lvn_list = []
 accepted_ops = ["const", "add", "print", "call", "mul", "id"]
+# bool_ops = ["le", "lte", "ge", "gte", "eq"]
 one_arg_ops = ["print", "ret"]
-ignore_ops = ["br", "jmp"]
+ignore_ops = ["br", "jmp", "speculate", "commit"]
 current_idx = 0
 offset = 0
 free_count = 0
@@ -213,6 +365,7 @@ def getVar2Num(val):
         return (val, None)
 
 def checkValInTable(val):
+    # print(lvn_list)
     for lvn in lvn_list:
         if val == lvn.value:
             return (True, lvn) 
@@ -230,6 +383,7 @@ def create_lvn_for_arg(instr, arg):
 def argument_checking(instr):
     arg_idx = []
     arg_repl = {}
+    # print(instr)
     for arg in instr["args"]:
         if arg in var2num:
             arg_idx.append(var2num[arg])
@@ -283,7 +437,7 @@ def createVal(instr, is_const): #TODO: Optimize this lol
                     return Table_Occ.NOT_IN_TABLE,lvn_comp
                 # print(lvn_comp)
                 return Table_Occ.REPLACE, lvn_comp
-            elif instr["op"] == "call":
+            elif instr["op"] == "call" or instr["op"] == "guard":
                 new_args, arg_idx = argument_checking(instr)
                 instr["args"] = new_args
                 comp_val = LVN_Value(instr["op"], (*arg_idx,))
